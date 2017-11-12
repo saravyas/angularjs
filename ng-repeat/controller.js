8fe3d49c813fc81fe53fbc9c0660d42271c67582
saravyas <saravyas007@gmail.com>
2017-11-12 10:16:10 +0530

    nested ng-repeat with table border

--- ng-repeat/controller.js
+++ ng-repeat/controller.js
@@ -5,15 +5,18 @@ function saraCtrl($scope){
 	var persons = [{
 		name : "sara",
 		age : "21",
-		mail : "saravyas007@gmail.com"
- 	},{
+		mail : "saravyas007@gmail.com",
+		degree: [{ug:"srec"},{pg:"IIT"}]
+	},{
 		name : "vyas",
 		age : "18",
-		mail : "vyas007@gmail.com"
- 	},{
+		mail : "vyas007@gmail.com",
+		degree: [{ug:"psg"},{pg:"NIT"}]
+	},{
 		name : "gomal",
 		age : "17",
-		mail : "gomalvyas007@gmail.com"
+		mail : "gomalvyas007@gmail.com",
+		degree: [{ug:"IIT"},{pg:"IISc"}]
 	}];
 
 	$scope.persons = persons;
