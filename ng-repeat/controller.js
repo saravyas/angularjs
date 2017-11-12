a16932ab2c3e823712da7c5fec110351a655fef0
saravyas <saravyas007@gmail.com>
2017-11-12 09:53:41 +0530

    ng-repeat example

---
+++ ng-repeat/controller.js
@@ -0,0 +1,20 @@
+var app = angular.module("saraApp",[])
+                 .controller("saraCtrl",saraCtrl);
+
+function saraCtrl($scope){
+	var persons = [{
+		name : "sara",
+		age : "21",
+		mail : "saravyas007@gmail.com"
+ 	},{
+		name : "vyas",
+		age : "18",
+		mail : "vyas007@gmail.com"
+ 	},{
+		name : "gomal",
+		age : "17",
+		mail : "gomalvyas007@gmail.com"
+	}];
+
+	$scope.persons = persons;
+}
