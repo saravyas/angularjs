a3f4a05b4dc1137245d9564f847c345de898bf6c
saravyas <saravyas007@gmail.com>
2017-11-12 21:28:41 +0530

    updating .catch promise

--- api_response_data/controller.js
+++ api_response_data/controller.js
@@ -1,19 +1,17 @@
 var app = angular.module("saraApp",[])
                  .controller("saraCtrl",saraCtrl);
 
-function saraCtrl($scope,$http){
+function saraCtrl($scope,$http,$log){
  	$http.get('https://jsonplaceholder.typicode.com/posts')
  		 .then(function(response){
  		 	$scope.persons = response.data;
  		 	console.log(response.data);
+ 		 	$log.info(response);
+ 		 })
+ 		 .catch(function(error){
+ 		 	$scope.error = error;
+ 		 	console.log(error);
+ 		 	$log.info(error)
  		 });
 };
-
-// function saraCtrl($scope,$http){
-// 	without using $http.get()
-//  	$http({method : 'GET',url : 'https://jsonplaceholder.typicode.com/posts'})
-//  		 .then(function(response){
-//  		 	$scope.persons = response.data;
-//  		 	console.log(response.data);
-//  		 });
-// };
+ 
